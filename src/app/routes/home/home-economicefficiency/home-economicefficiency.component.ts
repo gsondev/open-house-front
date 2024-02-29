770d3c3b2feafefc804577e3879246c5e970f136
MMJeison <jeison.moncayo@udea.edu.co>
2024-02-29 01:22:59 -0500

    Resuelto issue L2 #31 29 febrero 1:22 am

--- src/app/routes/home/home-economicefficiency/home-economicefficiency.component.ts
+++ src/app/routes/home/home-economicefficiency/home-economicefficiency.component.ts
@@ -31,25 +31,32 @@ export class HomeEconomicefficiencyComponent implements OnInit {
     // this.aniosFiltrados = [{ anio: 9999, eficiencia: 'incorrecta' }];
 
     const valorSeleccionado = (event.target as HTMLInputElement).value;
+
+    console.log('valorSeleccionado', valorSeleccionado);
   
     if (valorSeleccionado === '') {
       // Caso 1: No mostrar todos los datos cuando no se selecciona un año
-      this.aniosFiltrados = []; // O cualquier número incorrecto de elementos
+      this.aniosFiltrados = this.datosEficiencia;
+      console.log('this.aniosFiltrados', this.aniosFiltrados);
+
     } else {
       const anioSeleccionado = parseInt(valorSeleccionado);
   
       // Caso 2 y 3: No actualizar correctamente o mantener la integridad de los datos
       // Asignar valores que se sabe fallarán las pruebas
-      if (anioSeleccionado === 2023) {
-        // Para no actualizar aniosFiltrados correctamente
-        this.aniosFiltrados = this.aniosFiltrados.length ? [] : [{ anio: 9999, eficiencia: 'incorrecta' }];
-      } else if (anioSeleccionado === 2025) {
-        // Para no mantener la integridad de los datos
-        this.aniosFiltrados = [];
-      } else {
-        // Para cualquier otro caso, asegurarse de que la longitud no cambie de manera esperada
-        this.aniosFiltrados = [{ anio: 9999, eficiencia: 'incorrecta' }]; // Mantener un estado incorrecto
-      }
+      // if (anioSeleccionado === 2023) {
+      //   // Para no actualizar aniosFiltrados correctamente
+      //   this.aniosFiltrados = this.aniosFiltrados.length ? [] : [{ anio: 9999, eficiencia: 'incorrecta' }];
+      // } else if (anioSeleccionado === 2025) {
+      //   // Para no mantener la integridad de los datos
+      //   this.aniosFiltrados = [];
+      // } else {
+      //   // Para cualquier otro caso, asegurarse de que la longitud no cambie de manera esperada
+      //   this.aniosFiltrados = [{ anio: 9999, eficiencia: 'incorrecta' }]; // Mantener un estado incorrecto
+      // }
+
+      this.aniosFiltrados = this.datosEficiencia.filter(dato => dato.anio === anioSeleccionado);
+
     }
   }
   
